4219678a8dc13c43181424fde69c80010e00eb3b
Serge Bunas <Sergeibynas@gmail.com>
2025-03-31 14:04:29 +0700

    bumped version

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { CreateUiTool } from "./tools/create-ui.js";
 import { LogoSearchTool } from "./tools/logo-search.js";
 import { FetchUiTool } from "./tools/fetch-ui.js";
 
-const VERSION = "0.0.28";
+const VERSION = "0.0.29";
 
 const server = new McpServer({
   name: "21st-magic",
